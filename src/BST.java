37c41ae707482e5a82a4c21746766efd5fe160ff
Ns2ta <aldiyar018@gmail.com>
2026-04-23 15:44:06 +0500

    Commit 2

--- src/BST.java
+++ src/BST.java
@@ -1,23 +1,92 @@
 public class BST<K extends Comparable<K>, V> {
+
     private Node root;
-    private class Node;
+    private int size = 0;
+    private class Node
     {
         private K key;
         private V val;
-        private Node left, rigth;
+        private Node left, right;
+
         public Node(K key, V val)
         {
             this.key = key;
             this.val = val;
         }
     }
-    public void put(K key, V val) {}
+    public void put(K key, V val) {
+        if (root == null) {
+            root = new Node(key, val);
+            size++;
+            return;
+        }
 
-    public V get(K key) {}
+        Node parent = null;
+        Node current = root;
 
-    public void delete(K key) {}
+        while (current != null) {
+            parent = current;
+            int cmp = key.compareTo(current.key);
 
-    public Iterable<K> iterator() {}
+            if (cmp < 0) current = current.left;
+            else if (cmp > 0) current = current.right;
+            else {
+                current.val = val;
+                return;
+            }
+        }
+
+        int cmp = key.compareTo(parent.key);
+        if (cmp < 0) parent.left = new Node(key, val);
+        else parent.right = new Node(key, val);
+
+        size++;
+    }
+
+    public V get(K key) {
+        if (root == null) return;
+
+        Node current = root;
+        while (current != null) {
+            int cmp = key.compareTo(current.key);
+
+            if (cmp < 0) current = current.left;
+            else if (cmp > 0) current = current.right;
+            else {
+                return current.val;
+            }
+        }
+
+        return null;
+    }
+
+    public void delete(K key) {
+        if (root == null) return;
+
+        Node parent = null;
+        Node current = root;
+
+        while (current != null && !key.equals(current.key)) {
+            parent = current;
+
+            int cmp = key.compareTo(current.key);
+
+            if (cmp < 0) current = current.left;
+            else if (cmp > 0) current = current.right;
+            else {
+                current.val = null;
+                current.key = null;
+            }
+        }
+    }
+
+    public Iterable<K> iterator() {
+        //
+    }
+
+    public int size() {
+        return size;
+    }
 }
 
 //Add size
